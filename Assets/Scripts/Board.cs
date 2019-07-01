496c9b6997b39b1cc792e8e0b81f853d6caf3b4c
Ben Hicks <bh.benhicks@gmail.com>
2019-07-01 02:06:28 -0400

    Initial Commit
    Has Base Tile Implementation

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : Singleton<Board> {
+
+    public int nWidth;
+    public int nHeight;
+
+    public GameObject pfTile;
+    
+
+    public List<List<Tile>> lstTiles;
+
+
+    // i represents columns
+    // j represents rows in those columns
+    public void InitTiles() {
+        lstTiles = new List<List<Tile>>(nWidth);
+
+        for(int i=0; i<nWidth; i++) {
+
+            lstTiles.Add(new List<Tile>(nHeight));
+
+            for(int j=0; j<nHeight; j++) {
+
+                //only placing tiles in locations where the i and j coords have the same parity
+                if((i+j)%2 == 0) {
+                    GameObject goTile = Instantiate(pfTile, this.transform);
+                    lstTiles[i].Add(goTile.GetComponent<Tile>());
+                    lstTiles[i][j].Init(i, j);
+                } else {
+                    //No tile should exist at this i,j so we'll just put a dummy null value
+                    lstTiles[i].Add(null);
+                }
+
+            }
+
+        }
+    }
+
+    public override void Init() {
+        InitTiles();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour {
+
+    public Position pos;
+
+    public const float fTileDistHorz = 0.75f;
+    public const float fTileDistVert = 0.43333f;
+
+    public const float fTileGapHorz = 0.1f;
+    public const float fTileGapVert = 0.1f;
+
+    public int nI;
+    public int nJ;
+
+    public void Init(int i, int j) {
+        pos = new Position() {
+            i = i,
+            j = j
+        };
+
+        nI = i;
+        nJ = j;
+
+        PositionTile();
+    }
+
+    public void PositionTile() {
+
+        this.transform.localPosition = new Vector2(pos.i * fTileDistHorz + (pos.i - 1) * fTileGapHorz,
+                                                   pos.j * fTileDistVert + (pos.j - 1) * fTileGapVert);
+
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
