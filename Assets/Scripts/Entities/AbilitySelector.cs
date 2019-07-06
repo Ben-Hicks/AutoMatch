2510276bbf0e5463d174cef5e3f74127ea94adba
Ben Hicks <bh.benhicks@gmail.com>
2019-07-06 16:41:44 -0400

    Adding Entities/Abilities/AbilitySelection

---
+++ Assets/Scripts/Entities/AbilitySelector.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AbilitySelector : MonoBehaviour {
+
+    public Entity owner;
+
+
+    public abstract IEnumerator UseAbility();
+
+
+}
---
+++ Assets/Scripts/Entities/Entity.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity : MonoBehaviour {
+
+    public List<string> lstActions;//TO UPDATE
+    public int nMaxHealth;
+    public int nCurHealth;
+
+    public Tile tile;
+    public AbilitySelector abilityselector;
+
+    public void SetTile(Tile _tile) {
+        Debug.Assert(_tile != null);
+
+        tile = _tile;
+    }
+
+    public void SetAbilitySelector(AbilitySelector _abilityselector) {
+        abilityselector = _abilityselector;
+    }
+
+
+    // Start is called before the first frame update
+    void Start() {
+        abilityselector = GetComponent<AbilitySelector>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
